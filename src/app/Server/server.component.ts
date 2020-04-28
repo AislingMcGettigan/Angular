66514273f4202dbc2e37601b96760cc311285825
aisling mcgettigan <aislingmcgettigan93@hotmail.com>
2020-04-27 20:21:07 -0700

    ng style, ng class

--- src/app/Server/server.component.ts
+++ src/app/Server/server.component.ts
@@ -3,8 +3,26 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html'
+    ,styles:[`
+
+    .online{
+        color:white;
+    }
+    `]
 })
+
+
 export class ServerComponent{
 serverid = 10;
-status = 'offline';
+serverStatus = 'offline;'
+
+
+
+constructor(){
+this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+}
+
+getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red';
+}
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,10 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styles:[`
+
+    .online{
+        color:white;
+    }
+    `]
 })
 export class ServersComponent implements OnInit {
 userName = "";
+serverCreated =false;
+serverName = 'testServer';
+servers =[];
 
   constructor() { 
     
@@ -15,4 +23,16 @@ userName = "";
   ngOnInit(): void {
   }
 
+  onClick(){
+    this.serverCreated = true;
+    this.servers.push(this.servers.length + 1);
+  }
+
+  getColor(){
+    return this.servers.length > 5 ? 'blue':'red';
+  }
+
+  getClass(){
+    return this.servers.length > 5 ? 'online':'';
+  }
 }
